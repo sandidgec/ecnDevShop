062db9eb2c618953f37c11459ba0fc35fbcabd6d
marietab <mbuck12231989@gmail.com>
2016-05-17 10:15:49 -0600

    added mord files

---
+++ head-utils.php
@@ -0,0 +1,23 @@
+<meta charset="UTF-8">
+<title>All Saints Dev Shop</title>
+
+<link rel="stylesheet" type="text/css" href="lib/css/style.css" />
+<link href='http://fonts.googleapis.com/css?family=Open+Sans:400,600,300' rel='stylesheet' type='text/css'>
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+
+<link rel="stylesheet" href="https://ajax.googleapis.com/ajax/libs/jqueryui/1.11.2/themes/smoothness/jquery-ui.css" />
+
+<script src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.11.2/jquery-ui.min.js"></script>
+
+<script src="lib/js/jquery.stellar.js"></script>
+
+<script src="lib/js/scrip.js"></script>
+
+<script src="lib/js/jquery.nicescroll.js"></script>
+
+<link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css"
+      integrity="sha512-dTfge/zgoMYpP7QbHy4gWMEGsbsdZeCXz7irItjcC3sPUFtf0kuFbDz/ixG7ArTxmDjLXDmezHubeNikyKGVyQ=="
+      crossorigin="anonymous">
+
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.6.1/css/font-awesome.min.css">
--- index.php
+++ index.php
@@ -1,14 +1,105 @@
 <!DOCTYPE html>
-<html>
+<html lang="en">
+<head>
+    <?php require_once("lib/head-utils.php"); ?>
+</head>
 
-    <head>
+<nav class="navbar navbar-default">
+    <div class="container-fluid">
+        <!-- Brand and toggle get grouped for better mobile display -->
+        <div class="navbar-header">
+            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+                <span class="sr-only">Toggle navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+            <a class="navbar-brand" href="index.php">Home</a>
+        </div>
+        <!-- Collect the nav links, forms, and other content for toggling -->
+        <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+            <ul class="nav navbar-nav">
+                <li><a href="#missionState">Mission Statement</a></li>
+                <li><a href="#erd">ERD</a></li>
+                <li><a href="#aboutUs">About Us</a></li>
+                <li><a href="#wireFram">Wire Frame</a></li>
+                <li><a href="#useCs">Use Case</a></li>
+            </ul>
+        </div><!-- /.navbar-collapse -->
+    </div><!-- /.container-fluid -->
+</nav>
 
-    </head>
+<body>
 
-    <body>
-        <h1>Hello ECN DEV SHOP TEAM!</h1>
-        <h2> this is Marty Buck, dev shop team </h2>
-        <h2>This is Ryan, dev shop team</h2>
-        <h2>KRIEGGY BEARS is a horrible name</h2>
-    </body>
+<div id="wall_1" class="image" data-stellar-background-ratio="0.4" ></div>
+<div id="content_1" class="content">
+    <h1><a name="missionState">Mission Statement</a></h1>
+    <p>
+        Our job is to continually customize our data driven resources and effectively enhance our missions infrastructure.
+        We will develop and encourage personal employee growth, while we implement technical strategies. All the while,
+        We will effectively network to meet our missions needs
+    </p>
+</div>
+<a name="erd"></a>
+<div id="wall_2" class="image" data-stellar-background-ratio="0.4"></div>
+<div id="content_2" class="content" >
+
+    <h1><a name="aboutUs">About Us</a></h1>
+    <p>
+        With the Episcopal church, we are driven to provide web
+        based services to surrounding areas. At the Dev Shop, we make sure to
+        balance work and life, while providing an exciting, creative and
+        sustainable business environment for our team and community.
+    </p>
+</div>
+<a name="wireFram"></a>
+<div id="wall_3" class="image" data-stellar-background-ratio="0.4"  ></div>
+<div id="content_2" class="content">
+
+    <h1><a name="useCs">Use Case</a></h1>
+    <h2>Krieg</h2>
+    <p>
+        Meeting different artist at various art events, I hand them my
+        business card with our website to market our services for web development.
+        When the artist arrives to our website, they are greeted with a hero page;
+        containing a stylized navbar with links to our different pages (About, Services and Contact).
+        Customer goes to our "Services" pages to see what kind of services we provide.
+        Within the services page are listed services; CSS, PHP, HTML and Javascript.
+        Along with our sevices pages, is a brief description about what developer
+        specializes in that particular service offered.
+    </p>
+    <br>
+    <h2>Ryan</h2>
+    <p>
+        An emerging local produce shop is looking for ways to promote their
+        products. They begin researching ways to bring in more customers and
+        reach out  to potential customers. The business owner locates the
+        Development Web Shop online located in Farmington, New Mexico on
+        the grounds of the Episcopal Church of Navajoland. After searching
+        hours after hours the owner finally finds what he is searching for.
+        Contact information, resumes, and galleries of past projects are available
+        for all. A list of services such as wire-framing, site-mapping,
+        JavaScript, MySQL Database and web designed are presented. The produce
+        shop owner contacts the Development Web Shop and is confident the
+        Development Web Shop is highly capable of developing a website suited
+        for the needs of customers.
+    </p>
+    <br>
+    <h2>Sherri</h2>
+    <p>
+        A developer was on her way to a conference, she stops in at a irish
+        restaurant.  While she was eating,  she overheard a customer talking
+        about developing a customer database for a local church..  she noted
+        the enthusiasm in the tone.  Immediately, after eating, of course.
+        She approached the customer and inquired about what kind of developing
+        methods were involved. the developer was impressed about the idea..and
+        was immediately directed to the website.  She was impressed with the
+        site map and found the answers that she needed. In the morning before
+        she left town she stopped by the church and wrote a check because
+        she believed it was a good cause.
+    </p>
+</div>
+
+
+</body>
 </html>
