3bcb9cc50e037904d512b55477dd26904c03048c
Krieg Benally <kriegbenally@gmail.com>
2016-05-26 10:17:20 -0600

    updated "projectAssignment.php"

--- php/classes/projectAssignmentTesting.php
+++ php/classes/projectAssignmentTesting.php
@@ -1,2 +1,17 @@
 <?php
 
+require_once ("../../devshop.php");
+require_once (auto.php);
+
+
+
+
+
+// setup dsn and options
+$dsn = 'mysql:host=' . $config["hostname"] . ';dbname=' . $config["database"];
+$options = array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8");
+
+
+// set up pdo connection with database and set error attributes
+$pdo = new PDO($dsn, $config["username"], $config["password"], $options);
+$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
