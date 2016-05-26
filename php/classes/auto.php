0d65c15d00937576d421abf5ee0d114453ed758a
marietab <mbuck12231989@gmail.com>
2016-05-26 09:56:24 -0600

    test

--- php/classes/auto.php
+++ php/classes/auto.php
@@ -1,21 +1,18 @@
 <?php
-/**
- * automatically loads the class on demand
- *
- * @param string $className class name to load
- * @return bool true if the class loaded correctly, false if not
- **/
-function loadClass($className) {
-    $className[0] = strtolower($className[0]);
-    $className = preg_replace_callback("/([A-Z])/", function($matches) {
-        return("-" . strtolower($matches[0]));
-    }, $className);
-    $classFile = __DIR__ . "auto.php/" . $className . ".php";
-    if(is_readable($classFile) === true && require_once($classFile)) {
-        return(true);
-    } else {
-        return(false);
+spl_autoload_register("Autoloader::classLoader");
+class Autoloader {
+    /**
+     * This function autoloads classes if they exist
+     *
+     * @param string $className name of class to load
+     * @return bool false if classes can not be loaded
+     **/
+    public static function classLoader($className) {
+        $className = strtolower($className);
+        if(is_readable(__DIR__ . "/$className.php")) {
+            require_once(__DIR__ . "/$className.php");
+        } else {
+            return (false);
+        }
     }
 }
-// tell PHP to use the loadClass() function to automatically load class files
-spl_autoload_register("loadClass");
