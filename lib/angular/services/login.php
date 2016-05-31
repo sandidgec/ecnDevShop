cf66584898a82268dfc6127e5a063ffa1a8344fc
marietab <mbuck12231989@gmail.com>
2016-05-31 10:12:08 -0600

    added angular folder/folders

---
+++ lib/angular/services/login.php
@@ -0,0 +1,31 @@
+app.service("LoginService", function($http, $q) {
+this.LOGIN_ENDPOINT = "/~invtext/backend/php/api/user/";
+this.getUsersByEmail = function(email) {
+return ($http.get(this.LOGIN_ENDPOINT + "?email=" + email)
+.then(function(reply) {
+if(typeof reply.data === "object") {
+return (reply.data);
+} else {
+return ($q.reject(reply.data));
+}
+}, function(reply) {
+return ($q.reject(reply.data));
+}));
+};
+this.editUser = function(user) {
+return ($http.put(this.LOGIN_ENDPOINT + user.userId, user)
+.then(function(reply) {
+if(typeof reply.data === "object") {
+return (reply.data);
+} else {
+return ($q.reject(reply.data));
+}
+}, function(reply) {
+return ($q.reject(reply.data));
+}));
+};
+});
+
+
+
+
