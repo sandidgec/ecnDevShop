617219840ca20b64f7de03aa8de18265857c3d18
Krieg Benally <kriegbenally@gmail.com>
2016-05-31 23:42:10 -0600

    Reverted back to original site.

--- head-utils.php
+++ head-utils.php
@@ -1,32 +1,23 @@
-<link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/css/bootstrap.min.css">
-<link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/css/bootstrap-theme.min.css">
-<link rel="stylesheet" type="text/css" href="lib/css/fullPage.css" />
+<meta charset="UTF-8">
+<title>All Saints Dev Shop</title>
+
 <link rel="stylesheet" type="text/css" href="lib/css/style.css" />
-<!--[if lt IE 9]>
-<script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
-<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-<![endif]-->
+<link href='http://fonts.googleapis.com/css?family=Open+Sans:400,600,300' rel='stylesheet' type='text/css'>
 
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
 
+<link rel="stylesheet" href="https://ajax.googleapis.com/ajax/libs/jqueryui/1.11.2/themes/smoothness/jquery-ui.css" />
 
-<style type="text/css">
-    img.wp-smiley,
-    img.emoji {
-        display: inline !important;
-        border: none !important;
-        box-shadow: none !important;
-        height: 1em !important;
-        width: 1em !important;
-        margin: 0 .07em !important;
-        vertical-align: -0.1em !important;
-        background: none !important;
-        padding: 0 !important;
-    }
-</style>
-<script type='text/javascript' src='./lib/js/jquery/jquery.js'></script>
-<script type='text/javascript' src='./lib/js/jquery/jquery-migrate.min.js'></script>
-<script type='text/javascript' src='./lib/js/jquery/ui/core.min.js'></script>
-<script type='text/javascript' src='./lib/js/jquery/ui/effect.min.js'></script>
-<script type='text/javascript' src='//maxcdn.bootstrapcdn.com/bootstrap/3.3.2/js/bootstrap.min.js?ver=4.5.2'></script>
+<script src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.11.2/jquery-ui.min.js"></script>
 
-<!--    ./scripts.js-->
+<script src="lib/js/jquery.stellar.js"></script>
+
+<script src="lib/js/scrip.js"></script>
+
+<script src="lib/js/jquery.nicescroll.js"></script>
+
+<link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css"
+      integrity="sha512-dTfge/zgoMYpP7QbHy4gWMEGsbsdZeCXz7irItjcC3sPUFtf0kuFbDz/ixG7ArTxmDjLXDmezHubeNikyKGVyQ=="
+      crossorigin="anonymous">
+
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.6.1/css/font-awesome.min.css">
