cfe2c50cbeb0f950f166025aa7e5c5fb5fb677c6
Krieg Benally <kriegbenally@gmail.com>
2016-06-02 09:24:02 -0600

    added brief description and removed carousel.

--- index.php
+++ index.php
@@ -13,9 +13,9 @@
     <div id="whereShitgoes" class="container">
         <div class="row">
             <div class="col-md-8">
-                <h1>What is Cheii's DevShop</h1>
+                <h1>Cheii's DevShop</h1>
                 <p>
-                    Cheii's DevShop is determined to provide excellent mobile and web designs to neighboring communities
+                    Determined to providing excellent web and mobile designs to neighboring communities
                     &amp; businesses.
                 </p>
             </div>
@@ -35,76 +35,18 @@
 </header>
 
 
-<div id="carslide" class="container">
-    <div class="row" id="takeMeThere">
-        <div class="col-sm-12">
-            <div id="my-slider" class="carousel slide" data-ride="carousel">
 
-                <!-- indicators dot nov -->
-                <ol class="carousel-indicators">
-                    <li data-target="#my-slider" data-slide-to="0" class="active"></li>
-                    <li data-target="#my-slider" data-slide-to="1"></li>
-                    <li data-target="#my-slider" data-slide-to="2"></li>
-                    <li data-target="#my-slider" data-slide-to="3"></li>
-                    <li data-target="#my-slider" data-slide-to="4"></li>
-                    <li data-target="#my-slider" data-slide-to="5"></li>
-                    <li data-target="#my-slider" data-slide-to="6"></li>
-                </ol>
 
-                <!-- wrapper for slides-->
-                <div class="carousel-inner" role="listbox">
-                    <div class="item active">
-                        <img src="lib/images/jsgiffy.gif" alt="" />
-                        <div class="carousel-caption">
-                            <h1 class="text-center">Intro to Javascript</h1>
-                            <p class="text-center">
-                                <a class="btn btn-lg btn-primary" href="#" role="button">Learn More</a>
-                            </p>
-                        </div>
-                    </div>
-                    <div class="item">
-                        <img src="lib/images/render.gif" alt="javascript" />
-                        <div class="carousel-caption">
-                            <h1 class="text-center">More complex codding</h1>
-                        </div>
-                    </div>
-                    <div class="item">
-                        <img src="lib/images/plstrnOn.gif" alt="javascript" />
-                        <div class="carousel-caption">
-                            <h1 class="text-center">Hitting the power of JS</h1>
-                        </div>
-                    </div>
-                    <div class="item">
-                        <img src="lib/images/timer.gif" alt="javascript" />
-                        <div class="carousel-caption">
-                            <h1 class="text-center">It does require more time to code Java</h1>
-                        </div>
-                    </div>
-                    <div class="item">
-                        <img src="lib/images/jsSwitch.gif" alt="javascript" />
-                        <div class="carousel-caption">
-                            <h1 class="text-center">Dope</h1>
-                        </div>
-                    </div>
-                    <div class="item">
-                        <img src="lib/images/jscj.gif" alt="javascript" />
-                        <div class="carousel-caption">
-                            <h1 class="text-center">Javascript controls how your site acts</h1>
-                        </div>
-                    </div>
-                </div>
+<div id="learn-more" class="container info-content">
+    <div class="row">
+        <div class="col-md-12 text-center">
+            <h2 class="h2font2">What is Cheii's DevShop?</h2>
+        </div>
+    </div>
 
-                <!-- controls for next and prev buttons-->
-                <a class="left carousel-control" href="#my-slider" role="button" data-slide="prev">
-                    <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
-                    <span class="sr-only">Previous</span>
-                </a>
-                <a class="right carousel-control" href="#my-slider" role="button" data-slide="next">
-                    <span class="glyphicon glyphicon-chevron-right"></span>
-                    <span class="sr-only">Next</span>
-                </a>
-
-            </div>
+    <div class="row">
+        <div class="col-md-12 text-center">
+            <h2 class="h2font2">What services do we provide??</h2>
         </div>
     </div>
 </div>
@@ -112,22 +54,14 @@
 <div class="container">
     <div class="row">
         <div class="col-md-6">
-            <h1>What is Javascript?</h1>
             <p>
-                JavaScript is a cross-platform, object-oriented scripting language.
-                It is a small and lightweight language. Inside a host environment
-                (for example, a web browser), JavaScript can be connected to the
-                objects of its environment to provide programmatic control over them.
+                We are a web and mobile development shop.  We are the first of its kind on Navajo land.  Come to us and
+                we will provide you with the best and up to date development for you web site or mobile applications.
             </p>
         </div>
         <div class="col-md-6">
-            <h1>JavaScript and Java</h1>
             <p>
-                JavaScript and Java are similar in some ways but fundamentally
-                different in some others. The JavaScript language resembles Java but
-                does not have Java's static typing and strong type checking. JavaScript
-                follows most Java expression syntax, naming conventions and basic control-flow
-                constructs which was the reason why it was renamed from LiveScript to JavaScript.
+                
             </p>
         </div>
     </div>
