7691c14496b1664647053a1474a5b7bdef7470b0
Krieg Benally <kriegbenally@gmail.com>
2016-06-02 10:52:03 -0600

    Added CSS to navbar

--- index.php
+++ index.php
@@ -7,10 +7,10 @@
 
 <body>
 
-<?php require_once("navBar.php");?>
+<?php require_once("navBar2.php");?>
 
 <header>
-    <div id="whereShitgoes" class="container">
+    <div id="whereShitgoes" class="container">  
         <div class="row">
             <div class="col-md-8">
                 <h1>Cheii's DevShop</h1>
@@ -26,9 +26,9 @@
         </div>
 
 
-        <div class="row">
-            <div class="col-md-2 col-md-offset-5">
-                <button type="button" id="learnMoreBtn" class="btn btn-default"><a href="#takeMeThere"> LEARN MORE </a></button>
+        <div class="row" id="launchButton">
+            <div class="col-md-2 col-md-offset-8">
+                <a href="#takeMeThere"><button type="button" id="learnMoreBtn" class="btn btn-default">LEARN MORE</button></a>
             </div>
         </div>
     </div>
@@ -38,7 +38,7 @@
 <div id="learn-more" class="container info-content">
     <div class="row">
         <div class="col-md-12 text-center">
-            <h2 class="h2font2">What is Cheii's DevShop?</h2>
+            <h2 class="h2font2" id="takeMeThere">What is Cheii's DevShop?</h2>
         </div>
     </div>
 
@@ -49,23 +49,29 @@
     </div>
 </div>
 
-<div class="container">
+<div class="container" id="cheiiShop">
     <div class="row">
         <div class="col-md-6">
             <p>
                 We are a web and mobile development shop.  We are the first of its kind on Navajo land.  Come to us and
-                we will provide you with the best and up to date development for you web site or mobile applications.
+                we will provide you with the best and most up to date development for you web site or mobile applications.
             </p>
         </div>
         <div class="col-md-6">
             <p>
-                
+                Our staff are more than confident to take on any problem that you need solving.  With skill sets ranging
+                to front end customization, back end database knowledge, javascript, css and php.
             </p>
         </div>
     </div>
 </div>
 
 <script src="lib/js/glideScroll.js"></script>
+
+<footer>
+    footer
+</footer>
+
 
 </body>
 
---
+++ navBar2.php
@@ -0,0 +1,48 @@
+<nav class="navbar navbar-inverse no-border-radius">
+    <div class="container">
+        <!-- Brand and toggle get grouped for better mobile display -->
+        <div class="navbar-header">
+            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+                <span class="sr-only">Toggle navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+            <a class="navbar-brand" href="">
+                Cheii's DevShop
+            </a>
+        </div>
+
+        <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+            <ul class="nav navbar-nav navbar-right">
+                <li><a href="">about us</a></li>
+                <li><a href="">services</a></li>
+                <li><a href="">our team</a></li>
+                <li><a href="">contact us</a></li>
+<!--                <li class="dropdown">-->
+<!--                    <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">The Program <span class="caret"></span></a>-->
+<!--                    <ul class="dropdown-menu">-->
+<!--                        <li><a href="the-program">About</a></li>-->
+<!--                        <li><a href="careers">Careers</a></li>-->
+<!--                        <li><a href="contact">Contact/Team</a></li>-->
+<!--                        <li><a href="faq">FAQ</a></li>-->
+<!--                        <li><a href="partners">Partners</a></li>-->
+<!---->
+<!--                        <li role="separator" class="divider"></li>-->
+<!--                        <li>-->
+<!--                            <a href="image/cultivating-coders-press-kit.pdf" target="_blank">-->
+<!--                                Press Kit-->
+<!--                            </a>-->
+<!--                        </li>-->
+<!--                    </ul>-->
+<!--                </li>-->
+                <li><a href="" target="_blank">
+                        <i class="fa fa-twitter someicons" aria-hidden="true"></i>
+                    </a></li>
+                <li><a href="" target="_blank">
+                        <i class="fa fa-facebook someicons" aria-hidden="true"></i>
+                    </a></li>
+            </ul>
+        </div><!-- /.navbar-collapse -->
+    </div><!-- /.container-fluid -->
+</nav>
