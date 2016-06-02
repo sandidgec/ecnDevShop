61692046e8d1d85bfbf94afa41d919fb3dd18553
Ryan Sam <rnsam@asu.edu>
2016-06-02 15:25:12 -0600

    added ourTeam.php

--- services.php
+++ services.php
@@ -15,7 +15,7 @@
         <div class="jumbotron">
             <h1 class="text-center">Our Services</h1>
             <p class="text-center">We offer planning, creating, and maintaining web and mobile development. We strive to deliver up to date products in order to ensure the needs of our customers and their consumers.
-                                    we provide services to local and surrounding communities on and off the Navajo Navtion. </p>
+                                    we provide services to local and surrounding communities on and off the Navajo Nation. </p>
             <p class="text-center"><a class="btn btn-primary btn-lg" href="contact.php" role="button">Contact Us</a></p>
         </div>
         <div id="photoIcons" class="col-md-4">
