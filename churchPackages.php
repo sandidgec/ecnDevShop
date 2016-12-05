5bd0e7c464858dbef47b9d7e20c7836be1ee939c
marietab <mbuck12231989@gmail.com>
2016-12-05 12:27:13 -0700

    Decaon Package info

--- churchPackages.php
+++ churchPackages.php
@@ -8,6 +8,76 @@
 </head>
 
 <body>
+  <div id="bg">
+      <div id="search-container">
+          <div id="search-bg"></div>
+          <div id="search">
+              <div class="row">
+                  <div class="col-lg-4">
+                      <div class="thumbnail" id="clergy">
+                          <img src="/lib/images/developers/martyB.jpg">
+                      </div>
+                  </div>
+                  <div class="col-lg-8" id="text">
+                      <p>
+                          <h1>Deacon Packages</h1>
+                       <br>
+                      
+                          <h2>-3 Pages (Home, About Us, Contact [navigation bar]) Full Price Pages ($100)<br>
+                          <h3>Home<br>
+                            <h4>Image Carousel<br>
+                            <h4>3 images with links to pages<br>
+
+                          <h3>About Us (About, Welcome, Explore, etc.)<br>
+                            <h4>1 image<br>
+                            <h4>Text about Parish (how we started, who we are, our beliefs, etc.)<br>
+
+                          <h3>Contact
+                            <h4>Map of Church location<br>
+                            <h4>Contact Form<br>
+                            <h4>Address, phone, email & social media tags.
+
+
+
+
+                      </p>
+                  </div>
+              </div>
+          </div>
+      </div>
+  </div>
+  <br>
+
+  <div id="bg">
+      <div id="search-container">
+          <div id="search-bg"></div>
+          <div id="search">
+              <div class="row">
+                  <div class="col-lg-4">
+                      <div class="thumbnail" id="developers">
+                          <img src="/lib/images/developers/kriegB.png">
+                      </div>
+                  </div>
+                  <div class="col-lg-8" id="text">
+                      <p>
+                      <h1>Krieg Benally</h1>
+                      <br>
+                      <h2>-Photographer<br>
+                          -Videographer<br>
+                          -Developer<br>
+                          -Graphic Designer<br>
+                      </h2>
+                      </p>
+                  </div>
+              </div>
+          </div>
+      </div>
+  </div>
+
+</div>
+
+
+</div>
 
 
 
