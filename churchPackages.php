dba637527f2e82a3b95bee6537a4612dac41840d
marietab <mbuck12231989@gmail.com>
2016-12-05 17:12:16 -0700

    added css to churchPackages.php

--- churchPackages.php
+++ churchPackages.php
@@ -11,89 +11,109 @@
   <?php require_once("navBar.php");?>
   <br>
   <br>
-  <div id="packageContainer">
-      <div id="deaconPackage">
-                  <div class="col-lg-8" id="text">
-                      <p>
-                          <h1>Deacon Package-Small Congregation (6 Total Pages)</h1>
+  <br>
+  <div class="row">
+
+      <div id="deaconPackage" class="container">
+
+          <div class="col-lg-12">
+
+
+                          <h1 id="standOut">Deacon Package- Small Congregation (6 Total Pages)</h1>
                        <br>
                           <h2>Three Pages (Home, About Us, Contact [navigation bar]) <br>
                           <h3>Home<br>
-                            <h4>Image Carousel<br>
-                            <h4>Three images with links to pages<br>
+                            <h4>-Image Carousel<br>
+                            <h4>-Three images with links to pages<br>
 
                           <h3>About Us (About, Welcome, Explore, etc.)<br>
-                            <h4>One image<br>
-                            <h4>Text about Parish (how we started, who we are, our beliefs, etc.)<br>
+                            <h4>-One image<br>
+                            <h4>-Text about Parish (how we started, who we are, our beliefs, etc.)<br>
 
                           <h3>Contact
-                            <h4>Map of Church location<br>
-                            <h4>Contact Form<br>
-                            <h4>Address, phone, email & social media tags.
+                            <h4>-Map of Church location<br>
+                            <h4>-Contact Form<br>
+                            <h4>-Address, phone, email & social media tags.
 
                           <h3>Up to three sub pages with one image per page and text-If more pages are needed, see price range<br>
                             <h4>Can be drop down category off one of the above<br>
                             <h4>Can choose three or make up your own (donate, calendar, ministries, worshop, etc.)
-                  </div>
-              </div>
+
+                        </div>
+                      </div>
+                    </div>
+
+
 
               <br>
               <br>
               <br>
 
-              <div id="deaconPackage">
-                          <div class="col-lg-8" id="text">
-                              <p>
-                                  <h1>Vicar Packages-Cathedral (8 Total Pages)</h1>
+              <div class="row">
+
+                  <div id="vicarPackage" class="container">
+
+                      <div class="col-lg-12">
+
+                                  <h1 id="standOut">Vicar Packages- Cathedral (8 Total Pages)</h1>
                                <br>
                                   <h2>Five Pages (Home, About Us, Contact, plus two [navigation bar]) <br>
                                   <h3>Home<br>
-                                    <h4>Image Carousel<br>
-                                    <h4>Four images with links to pages<br>
+                                    <h4>-Image Carousel<br>
+                                    <h4>-Four images with links to pages<br>
 
                                   <h3>About Us (About, Welcome, Explore, etc.)<br>
-                                    <h4>One image<br>
-                                    <h4>Text about Parish (how we started, who we are, our beliefs, etc.)<br>
+                                    <h4>-One image<br>
+                                    <h4>-Text about Parish (how we started, who we are, our beliefs, etc.)<br>
 
                                   <h3>Contact
-                                    <h4>Map of Church location<br>
-                                    <h4>Contact Form<br>
-                                    <h4>Address, phone, email & social media tags.
+                                    <h4>-Map of Church location<br>
+                                    <h4>-Contact Form<br>
+                                    <h4>-Address, phone, email & social media tags.
 
                                   <h3>Add two additional to navigation bar (donate, ministries, worshop, etc.)<br>
 
                                   <h3>Up to three sub pages with 1 image per page and text-If more pages are needed, see price range<br>
                                     <h4>Can be drop down category off one of the above<br>
                                     <h4>Can choose three or make up your own (donate, calendar, ministries, worshop, etc.)
+
+                              </div>
+                            </div>
                           </div>
-                      </div>
+
+
+
 
                       <br>
                       <br>
                       <br>
 
-                      <div id="bishopPackage">
-                                  <div class="col-lg-8" id="text">
-                                      <p>
-                                          <h1>Bishop Packages-Diocese (14 Total Pages)</h1>
+
+                      <div class="row">
+
+                          <div id="bishopPackage" class="container">
+
+                              <div class="col-lg-12">
+
+                                          <h1 id="standOut">Bishop Packages- Diocese (14 Total Pages)</h1>
                                        <br>
                                           <h2>Eight Pages (Home, About Us, Contact, Find A Church. plus two [navigation bar]) <br>
                                           <h3>Home<br>
-                                            <h4>Image Carousel<br>
-                                            <h4>Three to six images with links to pages<br>
+                                            <h4>-Image Carousel<br>
+                                            <h4>-Three to six images with links to pages<br>
 
                                           <h3>About Us (About, Welcome, Explore, etc.)<br>
-                                            <h4>One image<br>
-                                            <h4>Text about Parish (how we started, who we are, our beliefs, etc.)<br>
+                                            <h4>-One image<br>
+                                            <h4>-Text about Parish (how we started, who we are, our beliefs, etc.)<br>
 
                                           <h3>Find A Church (Church Locator)<br>
-                                            <h4>Map of All Churches<br>
-                                            <h4>Text locations, address, website links<br>
+                                            <h4>-Map of All Churches<br>
+                                            <h4>-Text locations, address, website links<br>
 
                                           <h3>Contact
-                                            <h4>Map of Church location<br>
-                                            <h4>Contact Form<br>
-                                            <h4>Address, phone, email & social media tags.
+                                            <h4>-Map of Church location<br>
+                                            <h4>-Contact Form<br>
+                                            <h4>-Address, phone, email & social media tags.
 
                                           <h3>Add two additional to navigation bar (donate, ministries, worshop, etc.)<br>
                                           <h3>Add two additional linked anywhere or dropdown<br>
@@ -102,20 +122,11 @@
                                             <h4>Can be drop down category off one of the above<br>
                                             <h4>Can choose twenty or make up your own (donate, calendar, ministries, worshop, etc.)<br>
                                             <h4>Each category can have it's own page instead of being listed all on one page
-                                  </div>
-                              </div>
 
+                            </div>
+                          </div>
+                        </div>
 
-
-
-          </div>
-
-      </div>
-  </div>
-</div>
-
-
-</div>
 
 
 
