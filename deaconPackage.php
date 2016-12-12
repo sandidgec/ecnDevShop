ff2cf8b8f6de979f45b3439ae5226da1e7d51099
marietab <mbuck12231989@gmail.com>
2016-12-12 16:26:00 -0700

    Added bishop, deacon, and vicar package pages.

---
+++ deaconPackage.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <title>Deacon Package</title>
+    <?php require_once("lib/head-utils.php");?>
+    <link rel="stylesheet" href="lib/css/about.css">
+</head>
+
+<body>
+
+<?php require_once("navBar.php");?>
+
+
+</body>
+</html>
