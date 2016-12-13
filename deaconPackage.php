49d4d4c57e8d47717e17e044818be65a2edceedd
marietab <mbuck12231989@gmail.com>
2016-12-13 15:04:50 -0700

    Adding css to the deacon page

--- deaconPackage.php
+++ deaconPackage.php
@@ -13,42 +13,104 @@
 <br>
 <br>
 <br>
-<div class="row">
 
-    <div id="deaconPackage" class="container">
+            <!-- START THE PACKAGES -->
+            <div class="row">
 
-        <div class="col-lg-12">
+              <h1 id="standOut">Deacon Package- Small Congregation (6 Total Pages)</h1>
 
+              <hr class="featurette-divider">
 
-                        <h1 id="standOut">Deacon Package- Small Congregation (6 Total Pages)</h1>
-                     <br>
-                        <h2>In the Deacon's Package you will have three pages on the navigation bar: Home, About Us, and Contact Us. <br>
-                        <h3>Your Home page will be your first page on your website.<br>
-                          <h4>-On your home page, you have the option to have an image slider, those images are horizontal silder slides and usually with a momentum effect.<br>
-                          <h4>-There will be three images with links to pages.<br>
-
-                        <h3>The About Us page helps you describe or idenify your churches attributes.  You can use other titles; Welcome, Explore, etc.<br>
-                          <h4>-On your about page, there will be one image.<br>
-                          <h4>-Text about your Parish, like how we started, who we are, our beliefs, etc.<br>
-
-                        <h3>The Contact Us page will help your vistors find your locations and give them options of how to contact you.
-                          <h4>-On your contact page, there will be a map of your church location.<br>
-                          <h4>-Contact Form, where they can enter their contact information to and send to you.<br>
-                          <h4>-Your current address, phone, email & social media tags will be shown for their information.
-
-                        <h3>You will have up to three sub pages with one image per page and text. If more pages are needed, price may range.<br>
-                          <h4>There will be a drop down category off one of the above.<br>
-                          <h4>You can choose three or make up your own: donate, calendar, ministries, worship, etc.
-
-                      </div>
+                <div id="deaconPackage" class="container">
+                    <div class="churchPackages">
+                        <div class="row">
+                            <div class="col-md-3">
+                                <div>
+                                    <img src="/lib/images/churchPackages/home.png">
+                                </div>
+                            </div>
+                            <div class="col-md-9">
+                                <p class="lead">
+                                   <h3>In the Deacon's Package you will have three pages on the navigation bar: Home, About Us, and Contact Us. <br>
+                                   <h3>Your Home page will be your first page on your website.<br>
+                                   <h3>On your home page, you have the option to have an image slider, those images are horizontal silder slides and usually with a momentum effect.<br>
+                                   <h3>There will be three images with links to pages.<br>
+                                </p>
+                            </div>
+                        </div>
                     </div>
-                  </div>
+                </div>
+
+                <hr class="featurette-divider">
 
 
+                <div id="deaconPackage" class="container">
+                  <div class="churchPackages">
+                      <div class="row">
+                            <div class="col-md-9">
+                                <p class="lead">
+                                  <h3>The About Us page helps you describe or idenify your churches attributes.  You can use other titles; Welcome, Explore, etc.<br>
+                                  <h3>On your about page, there will be one image.<br>
+                                  <h3>Text about your Parish, like how we started, who we are, our beliefs, etc.<br>
+                                </p>
+                            </div>
+                            <div class="col-md-3">
+                                <img src="/lib/images/churchPackages/about.png">
+                            </div>
+                        </div>
+                    </div>
+                </div>
 
-            <br>
-            <br>
-            <br>
+                <hr class="featurette-divider">
+
+                <div id="deaconPackage" class="container">
+                    <div class="churchPackages">
+                        <div class="row">
+                            <div class="col-md-3">
+                                <div>
+                                    <img src="/lib/images/churchPackages/contact.png">
+                                </div>
+                            </div>
+                            <div class="col-md-9">
+                                <p class="lead">
+                                  <h3>The Contact Us page will help your vistors find your locations and give them options of how to contact you.
+                                  <h3>On your contact page, there will be a map of your church location.<br>
+                                  <h3>Contact Form, where they can enter their contact information to and send to you.<br>
+                                  <h3>Your current address, phone, email & social media tags will be shown for their information.
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+
+                <hr class="featurette-divider">
+
+                <div id="deaconPackage" class="container">
+                  <div class="churchPackages">
+                      <div class="row">
+                            <div class="col-md-9">
+                                <p class="lead">
+                                  <h3>You will have up to three sub pages with one image per page and text. If more pages are needed, price may range.<br>
+                                  <h3>There will be a drop down category off one of the above.<br>
+                                  <h3>You can choose three or make up your own: donate, calendar, ministries, worship, etc.
+                                </p>
+                            </div>
+                            <div class="col-md-3">
+                                <img src="/lib/images/churchPackages/service.png">
+
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <br>
+                <br>
+                <br>
+
+                <!-- /END THE FEATURETTES -->
 
 </body>
+
+
 </html>
