5ab1c5d70e2819090df0512438238b46550ff4ee
marietab <mbuck12231989@gmail.com>
2016-12-20 09:52:57 -0700

    Working on css

--- deaconPackage.php
+++ deaconPackage.php
@@ -26,11 +26,6 @@
                     <div class="churchPackages">
                         <div class="row">
 
-                          <h2 id="standOut1">Home</h2>
-
-                          <p class="lead1">
-                             <h3>In the Deacon's Package you will have three pages on the navigation bar: Home, About Us, and Contact Us.
-                          </p>
 
                             <div class="col-md-6">
                                 <div>
@@ -38,6 +33,10 @@
                                 </div>
                             </div>
                             <div class="col-md-6">
+                              <h2 id="standOut1">Home</h2>
+                                <p class="lead1">
+                                    <h3>In the Deacon's Package you will have three pages on the navigation bar: Home, About Us, and Contact Us.
+                                </p>
                                 <p class="lead">
                                    <h3>On your home page, you have the option to have an image slider, those images are horizontal silder slides and usually with a momentum effect.<br>
                                    <h3>There will be three images with links to pages.<br>
@@ -51,9 +50,8 @@
                   <div class="churchPackages">
                       <div class="row">
 
-                        <h2 id="standOut1">About</h2>
-
                             <div class="col-md-6">
+                                <h2 id="standOut1">About</h2>
                                 <p class="lead">
                                   <h3>The About Us page helps you describe or idenify your churches attributes.  You can use other titles; Welcome, Explore, etc.<br>
                                   <h3>On your about page, there will be one image.<br>
@@ -71,14 +69,13 @@
                     <div class="churchPackages">
                         <div class="row">
 
-                          <h2 id="standOut1">Contact</h2>
-
                             <div class="col-md-6">
                                 <div>
                                     <img src="/lib/images/churchPackages/contact.JPG" alt="contact" width=550"" height="600">
                                 </div>
                             </div>
                             <div class="col-md-6">
+                                <h2 id="standOut1">Contact</h2>
                                 <p class="lead">
                                   <h3>The Contact Us page will help your vistors find your locations and give them options of how to contact you.
                                   <h3>On your contact page, there will be a map of your church location.<br>
@@ -94,9 +91,8 @@
                   <div class="churchPackages">
                       <div class="row">
 
-                        <h2 id="standOut1"></h2>
-
                             <div class="col-md-6">
+                              <h2 id="standOut1"></h2>
                                 <p class="lead">
                                   <h3>You will have up to three sub pages with one image per page and text. If more pages are needed, price may range.<br>
                                   <br>
