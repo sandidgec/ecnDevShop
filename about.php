f2cc86fde5d4eb7b1b846907cdfd980faae2cddd
marietab <mbuck12231989@gmail.com>
2017-07-25 10:00:03 -0600

    Editing
    Changing the information on the meet the team

--- about.php
+++ about.php
@@ -15,49 +15,6 @@
 
 <br>
 <br>
-
-
-<div class="row">
-
-    <div id="meetTheTeam" class="container">
-
-
-        <div class="col-md-10">
-
-            <h1 id="standOut">Our Core Values</h1>
-
-  <br>
-
-  <p>
-  <h2>To be Client centered:
-  </h2>
-Our tag line is “Let us help you tell your story.” We respect our clients, understand their needs and wants, and listen. Serving our clients is our highest priority.
-To continue to pursue growth and knowledge and share it:
-Cheii’s never stop learning. We look for ways to challenge ourselves to learn, and grow. We spend time watching video tutorials, business lectures and slide presentations. When we collectively teach each other, we all stand to gain from it.
-  <h2>Be Team-Oriented:
-  </h2>
-Great teams are made up of great people. We help each other, we collaborate, and we celebrate our achievements.
-Build a positive team and family spirit:
-Our best ideas are made when we are surrounded by people we care for. Having a positive and inviting attitude inspires everyone around. We share this family spirit with clients and strive to build relationships with everyone we meet and work with.
-  <h2>Humanity:
-  </h2>
-We believe that is our duty to serve our Church’s Mission. A portion of our earnings go to the Church for outreach and ministries.
-  <h2>To Communicate openly and honestly:
-  </h2>
-When interacting with others, we encourage open communication and honesty. This leads to strong relationships built on trust.
-  <h2>To be one with our Navajo Culture:
-  </h2>
-We understand our Navajo culture and share when possible. We understand that culture and technology can go hand in hand.
-  <h2>To care for all of creation:
-  </h2>
-Take care of the company and clients as they were one’s own family. This also means taking care of creation, which is part of our culture. By taking care of creation, we: Reduce, Reuse, and Recycle.
-
-    </div>
-  </div>
-</div>
-
-  </p>
-
 <br>
 <br>
 
@@ -105,9 +62,11 @@ Take care of the company and clients as they were one’s own family. This also 
                                 <p>
                                 <h1>Ryan Sam </h1>
                                 <br>
-                                <h2>-Photographer<br>
-                                    -Server Administrator<br>
-                                    -Developer<br>
+                                Financial Administrator
+                                Junior Web Developer
+                                Photographer
+                                <br>
+                                <h2> Born and raised in Ft. Defiance AZ. With a Bachelor of Science Degree in Criminal Justice and a new-found passion for Web Development.
                                 </h2>
                                 </p>
                             </div>
@@ -132,10 +91,11 @@ Take care of the company and clients as they were one’s own family. This also 
                                 <p>
                                 <h1>GJ Gordy</h1>
                                 <br>
-                                <h2>-Web Consultant<br>
-                                    -Graphic Designer<br>
-                                    -Developer<br>
-                                    -Managing Editor<br>
+                                Multimedia writer
+                                Junior Web Developer
+                                Graphic Designer
+                                <br>
+                                <h2> Born in Phoenix AZ and spent most her life in Coal Mine NM and Phoenix AZ. She is a mother of her three-year-old daughter Elizabeth. She has over 10 years of experience in early childhood development in various non-profit organizations and loves web development and the business aspect of the Web Development Shop.
                                 </h2>
                                 </p>
                             </div>
@@ -160,11 +120,11 @@ Take care of the company and clients as they were one’s own family. This also 
                                 <p>
                                 <h1>Marty Buck</h1>
                                 <br>
-                                <h2>
-                                    -Client Account Specialist<br>
-                                    -Graphic Designer<br>
-                                    -UX Designer<br>
-                                    -Developer<br>
+                                Etsy Specialist
+                                Junior Web Developer
+                                Graphic Designer
+                                <br>
+                                <h2> Born in Ft. Defiance and raised in Coal Mine NM. A mother of a four-year daughter named Mackenzie and newborn baby girl name Madison. She is a certified Nursing Assistant who loves the creativity of graphic design in Photoshop.
                                 </h2>
                                 </p>
                             </div>
@@ -207,6 +167,57 @@ Take care of the company and clients as they were one’s own family. This also 
         </div>
     <br>
     <br>
+
+    <br>
+    <br>
+    <br>
+
+
+    <div class="row">
+
+        <div id="meetTheTeam" class="container">
+
+
+            <div class="col-md-10">
+
+                <h1 id="standOut">Our Core Values</h1>
+
+      <br>
+
+      <p>
+      <h2>To be Client centered:
+      </h2>
+    Our tag line is “Let us help you tell your story.” We respect our clients, understand their needs and wants, and listen. Serving our clients is our highest priority.
+    To continue to pursue growth and knowledge and share it:
+    Cheii’s never stop learning. We look for ways to challenge ourselves to learn, and grow. We spend time watching video tutorials, business lectures and slide presentations. When we collectively teach each other, we all stand to gain from it.
+      <h2>Be Team-Oriented:
+      </h2>
+    Great teams are made up of great people. We help each other, we collaborate, and we celebrate our achievements.
+    Build a positive team and family spirit:
+    Our best ideas are made when we are surrounded by people we care for. Having a positive and inviting attitude inspires everyone around. We share this family spirit with clients and strive to build relationships with everyone we meet and work with.
+      <h2>Humanity:
+      </h2>
+    We believe that is our duty to serve our Church’s Mission. A portion of our earnings go to the Church for outreach and ministries.
+      <h2>To Communicate openly and honestly:
+      </h2>
+    When interacting with others, we encourage open communication and honesty. This leads to strong relationships built on trust.
+      <h2>To be one with our Navajo Culture:
+      </h2>
+    We understand our Navajo culture and share when possible. We understand that culture and technology can go hand in hand.
+      <h2>To care for all of creation:
+      </h2>
+    Take care of the company and clients as they were one’s own family. This also means taking care of creation, which is part of our culture. By taking care of creation, we: Reduce, Reuse, and Recycle.
+
+        </div>
+      </div>
+    </div>
+
+      </p>
+
+    <br>
+    <br>
+    <br>
+
 
 
 
